7b9e17d01bd80357b465870b65ddab5edb0f62ce
chenxu565 <106840474+chenxu565@users.noreply.github.com>
2022-06-04 19:48:39 +0000

    Add js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+/*import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
@@ -8,3 +8,25 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+*/
+
+if (document.readyState !== "loading") {
+  console.log("Document is ready");
+  initialzeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document ready after waiting");
+    initialzeCode();
+  });
+}
+
+function initialzeCode() {
+  const commentButton = document.getElementById("add-comment");
+
+  commentButton.addEventListener("click", function () {
+    const commentList = document.getElementById("comments");
+    let newParagraph = document.createElement("li");
+    newParagraph.innerHTML = "comment";
+    commentList.appendChild(newParagraph);
+  });
+}
